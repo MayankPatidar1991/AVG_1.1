6afd8f2904a0da5f3fe396cdcd6a881ddb8239d0
MayankPatidar1991 <make.patidar11@gmail,com>
2017-12-25 22:21:48 +0530

    Initial commit

---
+++ src/Generic/BaseTest.java
@@ -0,0 +1,56 @@
+package Generic;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class BaseTest implements IAutoConst
+{
+   
+public WebDriver driver;
+    static
+    {
+    	System.setProperty(CHROME_KEY, CHROME_VALUE);
+    	System.setProperty(GECKO_KEY, GECKO_VALUE);
+    }
+    @Parameters({"browser"})
+    @BeforeMethod(alwaysRun=true)
+     public void openApp(String browser)
+    {
+       if(browser.equals("chrome"))
+        {
+    	driver=new ChromeDriver();
+        }
+       else
+       {
+    	   driver=new FirefoxDriver();
+       }
+      String url=AL.getproperty(SETTINGS_PATH,"URL");
+          driver.get(url);
+      String strITO =AL.getproperty(SETTINGS_PATH, "ITO");
+               long ITO=Long.parseLong(strITO);
+          driver.manage().timeouts().implicitlyWait(ITO,TimeUnit.SECONDS);
+       }
+          
+    @AfterMethod(alwaysRun=true)
+    public void closeApp(ITestResult res)
+    {
+     String name=res.getName();
+     int Status= res.getStatus();
+     if(Status==2)
+     {
+    	 String imgpath = AL.getphoto(driver, PHOTO_PATH, name);
+    	 Reporter.log("Imagepath: "+imgpath,true);
+     }
+    	driver.quit();
+    }
+}
+
